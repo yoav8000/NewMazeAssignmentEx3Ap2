1123a2684c905f98c1d4f344e47c550f7c6bee03
yoav gomberg <yoav gomberg@DESKTOP-UQ08A7L>
2017-06-18 23:38:47 +0300

    succeeded in adding a user to the database after long hours of troubles with the data binding caused by a damm typo

--- NewMazeAssignmentEx3Ap2/NewMazeAssignmentEx3Ap2/Views/Scripts/Registration.js
+++ NewMazeAssignmentEx3Ap2/NewMazeAssignmentEx3Ap2/Views/Scripts/Registration.js
@@ -12,3 +12,65 @@ function validatePassword() {
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+
+
+
+
+var ViewModel = function () {
+    var self = this; // make 'this' available to subfunctions or closures
+    self.users = ko.observableArray(); // enables data binding
+    self.userName = ko.observable();
+    self.password = ko.observable();
+    self.email = ko.observable();
+    var usersUri = "/api/Users";
+
+    function getAllUsers() {
+        $.getJSON(usersUri).done(function (data) {
+            self.users(data);
+        });
+    }
+
+
+    self.checkIfUserNameExis = function () {
+        var tempUser = ko.utils.arrayFirst(self.users(), function (tempUser) {
+            return user.userName == tempUser.userName;
+        });
+        if (tempUser != null) {
+            alert("user name already exist in the system");
+        }
+    }
+
+
+
+    self.addUser = function () {
+        var usersUri = "/api/Users";
+
+        var tempUserName = document.getElementById("userName").value;
+        var user =
+            {
+                name: tempUserName,
+                password: document.getElementById("password").value,
+                email: document.getElementById("email").value,
+                Wins: 0,
+                Losses: 0
+            };
+
+        usersUri += "/" + tempUserName;
+
+
+        self.checkIfUserNameExis();
+
+       
+          
+                $.post(usersUri, user).done(function (item) {
+                    self.users.push(item);
+                    sessionStorage.setItem("userName", user.userName);
+                    //     window.location.replace("HomePage.html");
+                });
+
+       
+    }
+
+
+}
+ko.applyBindings(new ViewModel()); // sets up the data binding
